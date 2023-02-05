161d2a74f8f191d0bff23280134346d73ca9c805
MinetaS <skdty87@gmail.com>
2023-02-05 23:26:22 +0900

    Attempt 3: From 20.8% (rootDepth <= 4) to 70.0% (rootDepth >= 32)

--- src/search.cpp
+++ src/search.cpp
@@ -1267,7 +1267,7 @@ moves_loop: // When in check, search starts here
           if (rm.averageScore != -VALUE_INFINITE)
           {
               // Trust value more if depth is high.
-              int t = 682 + 19 * std::clamp(thisThread->rootDepth - 18, 0, 18);
+              int t = 213 + 18 * std::clamp(thisThread->rootDepth - 4, 0, 28);
               rm.averageScore =  (t * value + (1024 - t) * rm.averageScore)
                                / 1024;
           }
