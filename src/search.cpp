a51618192ce120c0927831acc53ebdae05ac45cc
MinetaS <skdty87@gmail.com>
2023-02-09 21:27:47 +0900

    Skip check extension if position is in check

--- src/search.cpp
+++ src/search.cpp
@@ -1105,7 +1105,8 @@ moves_loop: // When in check, search starts here
           // Check extensions (~1 Elo)
           else if (   givesCheck
                    && depth > 9
-                   && abs(ss->staticEval) > 78)
+                   && abs(ss->staticEval) > 78
+                   && !ss->inCheck)
               extension = 1;
 
           // Quiet ttMove extensions (~1 Elo)
