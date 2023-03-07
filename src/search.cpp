0807e8d2c82e48cf96a59c3b51e9073bfe0dda17
MinetaS <skdty87@gmail.com>
2023-03-07 13:03:22 +0900

    Latest LTC pass by Vizvezdenec (6405abda2644b62c33937119)

--- src/search.cpp
+++ src/search.cpp
@@ -1107,7 +1107,7 @@ moves_loop: // When in check, search starts here
                   extension = -2;
 
               // If the eval of ttMove is less than value, we reduce it (negative extension)
-              else if (ttValue <= value)
+              else if (ttValue <= value || ttValue <= alpha)
                   extension = -1;
           }
 
