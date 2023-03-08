70bf9d3ba01e84131e932512799131da86a8e849
MinetaS <skdty87@gmail.com>
2023-03-08 23:31:22 +0000

    Attempt 2: Mix singularBeta and r
    Bench: 5115046

--- src/search.cpp
+++ src/search.cpp
@@ -1071,8 +1071,8 @@ moves_loop: // When in check, search starts here
               && (tte->bound() & BOUND_LOWER)
               &&  tte->depth() >= depth - 3)
           {
-              Value singularBeta = ttValue - (2 + (ss->ttPv && !PvNode)) * depth;
-              Depth singularDepth = std::max((depth - 1) / 2 - (r == 0), 1);
+              Value singularBeta = ttValue - (2 - (r > 0) + (!PvNode && ss->ttPv)) * depth;
+              Depth singularDepth = (depth - 1) / 2;
 
               ss->excludedMove = move;
               value = search<NonPV>(pos, ss, singularBeta - 1, singularBeta, singularDepth, cutNode);
