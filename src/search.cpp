e7ebaa31c11b2bd672c3c6e41ca93b98525cf552
MinetaS <skdty87@gmail.com>
2023-03-15 11:47:53 +0900

    Retry #4183 (best mates: 2319)
    Bench: 4736649

--- src/search.cpp
+++ src/search.cpp
@@ -1191,7 +1191,8 @@ moves_loop: // When in check, search starts here
                      - 4182;
 
       // Decrease/increase reduction for moves with a good/bad history (~30 Elo)
-      r -= ss->statScore / (11791 + 3992 * (depth > 6 && depth < 19));
+      r -=  (ss->statScore + 5 * std::min(alpha, VALUE_KNOWN_WIN))
+          / (11791 + 3992 * (depth > 6 && depth < 19));
 
       // Step 17. Late moves reduction / extension (LMR, ~117 Elo)
       // We use various heuristics for the sons of a node after the first son has
