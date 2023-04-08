faade0f8385344ed92577c5628869817a18ef4c9
MinetaS <skdty87@gmail.com>
2023-04-08 00:28:17 +0000

    SEE + optimism
    Bench: 3504948

--- src/search.cpp
+++ src/search.cpp
@@ -1057,7 +1057,7 @@ moves_loop: // When in check, search starts here
               lmrDepth = std::max(lmrDepth, 0);
 
               // Prune moves with negative SEE (~4 Elo)
-              if (!pos.see_ge(move, Value(-24 * lmrDepth * lmrDepth - 16 * lmrDepth)))
+              if (!pos.see_ge(move, Value(std::min((-24 * lmrDepth - 16 + thisThread->optimism[us]) * lmrDepth, 0))))
                   continue;
           }
       }
