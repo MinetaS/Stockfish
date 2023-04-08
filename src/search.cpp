8991dd5d6ca49311842b920e3258910027121f35
MinetaS <skdty87@gmail.com>
2023-04-08 00:07:03 +0000

    Attempt 2: Simplification 1
    Bench: 3809335

--- src/search.cpp
+++ src/search.cpp
@@ -1322,7 +1322,6 @@ moves_loop: // When in check, search starts here
 
       if (value > bestValue)
       {
-          Value oldBestValue = bestValue;
           bestValue = value;
 
           if (value > alpha)
@@ -1336,17 +1335,10 @@ moves_loop: // When in check, search starts here
               {
                   // Reduce other moves if we have found at least one score improvement (~1 Elo)
                   if (   depth > 1
-                      && (   (improving && complexity > 971)
-                          || value < (5 * alpha + 75 * beta) / 87
-                          || depth < 6)
+                      && (value < (5 * alpha + 75 * beta) / 87 || depth < 6)
                       && beta  <  12535
                       && value > -12535)
-                  {
-                      bool extraReduction =   oldBestValue != -VALUE_INFINITE
-                                           && value - oldBestValue > (7 * (beta - alpha)) / 8
-                                           && depth > 2;
-                      depth -= 1 + extraReduction;
-                  }
+                      depth -= 1;
 
                   assert(depth > 0);
                   alpha = value;
