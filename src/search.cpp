b63cc6de59cd8d685540dfb798599e1162177e85
MinetaS <skdty87@gmail.com>
2023-04-11 15:11:42 +0000

    Tweak depth condition of PV improvement reduction
    Bench: 4443871

--- src/search.cpp
+++ src/search.cpp
@@ -1339,9 +1339,7 @@ moves_loop: // When in check, search starts here
               {
                   // Reduce other moves if we have found at least one score improvement (~1 Elo)
                   if (   depth > 1
-                      && (   (improving && complexity > 971)
-                          || value < (5 * alpha + 75 * beta) / 87
-                          || depth < 6)
+                      && depth < 6 + 6 * (value < (5 * alpha + 75 * beta) / 87)
                       && beta  <  12535
                       && value > -12535)
                       depth -= 1;
