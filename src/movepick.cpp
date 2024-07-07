ce2d782f1e5aa89f3d765650a83c60583b9f2b15
MinetaS <skdty87@gmail.com>
2024-07-07 13:38:00 +0900

    Attempt 5: Keep scale, weight equally between stm/opp
    Bench: 1061605

--- src/movepick.cpp
+++ src/movepick.cpp
@@ -179,8 +179,8 @@ void MovePicker::score() {
             // histories
             m.value = (*mainHistory)[pos.side_to_move()][m.from_to()];
             m.value += 2 * (*pawnHistory)[pawn_structure_index(pos)][pc][to];
-            m.value += 2 * (*continuationHistory[0])[pc][to];
-            m.value += 2 * (*continuationHistory[1])[pc][to];
+            m.value +=
+              3 * ((*continuationHistory[0])[pc][to] + (*continuationHistory[1])[pc][to]) / 2;
             m.value += (*continuationHistory[2])[pc][to] / 3;
             m.value += (*continuationHistory[3])[pc][to];
             m.value += (*continuationHistory[5])[pc][to];
