33d691132b12ad1365f68d761874a6bb8044c98c
MinetaS <skdty87@gmail.com>
2024-07-19 17:47:45 +0900

    Attempt 2: Add a depth condition to doShallowerSearch
    Bench: 1405507

--- src/search.cpp
+++ src/search.cpp
@@ -1197,7 +1197,7 @@ moves_loop:  // When in check, search starts here
                 // good enough search deeper, if it was bad enough search shallower.
                 const bool doDeeperSearch = value > (bestValue + 35 + 2 * newDepth);  // (~1 Elo)
                 const bool doShallowerSearch =
-                  value < bestValue + std::min(newDepth, 10);  // (~2 Elo)
+                  newDepth > 10 && value < bestValue + newDepth;  // (~2 Elo)
 
                 newDepth += doDeeperSearch - doShallowerSearch;
 
