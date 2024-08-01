e22502046917605c6b941bf022a93bb96b8302c5
MinetaS <skdty87@gmail.com>
2024-08-01 12:41:59 +0900

    Replace qsearch with shallow depth search in ProbCut
    Bench: 1359804

--- src/search.cpp
+++ src/search.cpp
@@ -859,10 +859,10 @@ Value Search::Worker::search(
             thisThread->nodes.fetch_add(1, std::memory_order_relaxed);
             pos.do_move(move, st);
 
-            // Perform a preliminary qsearch to verify that the move holds
-            value = -qsearch<NonPV>(pos, ss + 1, -probCutBeta, -probCutBeta + 1);
+            // Perform a preliminary shallow search to verify that the move holds
+            value = -search<NonPV>(pos, ss + 1, -probCutBeta, -probCutBeta + 1, depth / 16, !cutNode);
 
-            // If the qsearch held, perform the regular search
+            // If the previous search held, perform the regular search
             if (value >= probCutBeta)
                 value =
                   -search<NonPV>(pos, ss + 1, -probCutBeta, -probCutBeta + 1, depth - 4, !cutNode);
