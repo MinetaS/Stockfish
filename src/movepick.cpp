9863c228f7f2a1e9905538d19158d1da16d63784
MinetaS <skdty87@gmail.com>
2024-08-05 11:47:50 +0900

    Attempt 2: Keep priority of all capture moves
    Bench: 1319594

--- src/movepick.cpp
+++ src/movepick.cpp
@@ -178,9 +178,9 @@ void MovePicker::score() {
 
         else  // Type == EVASIONS
         {
-            if (pos.capture_stage(m) && pos.see_ge(m))
-                m.value =
-                  PieceValue[pos.piece_on(m.to_sq())] - type_of(pos.moved_piece(m)) + (1 << 28);
+            if (pos.capture_stage(m))
+                m.value = PieceValue[pos.piece_on(m.to_sq())] - type_of(pos.moved_piece(m))
+                        + (1 << (27 + pos.see_ge(m)));
             else
                 m.value = (*mainHistory)[pos.side_to_move()][m.from_to()]
                         + (*continuationHistory[0])[pos.moved_piece(m)][m.to_sq()]
