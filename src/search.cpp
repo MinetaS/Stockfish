8e7f1c2f32d4e0a7bc43e34ea6dcb833785614fa
MinetaS <skdty87@gmail.com>
2024-08-26 04:50:25 +0900

    NMP experiment
    Bench: 1206145

--- src/search.cpp
+++ src/search.cpp
@@ -776,30 +776,33 @@ Value Search::Worker::search(
         assert(eval - beta >= 0);
 
         // Null move dynamic reduction based on depth and eval
-        Depth R = std::min(int(eval - beta) / 202, 6) + depth / 3 + 5;
+        const int   R            = std::min(int(eval - beta) / 202, 6) + depth / 3;
+        const Depth nmpReduction = R + 5;
+        const Value nmpBeta      = beta + R * 2;
 
         ss->currentMove         = Move::null();
         ss->continuationHistory = &thisThread->continuationHistory[0][0][NO_PIECE][0];
 
         pos.do_null_move(st, tt);
 
-        Value nullValue = -search<NonPV>(pos, ss + 1, -beta, -beta + 1, depth - R, false);
+        Value nullValue =
+          -search<NonPV>(pos, ss + 1, -nmpBeta, -nmpBeta + 1, depth - nmpReduction, false);
 
         pos.undo_null_move();
 
         // Do not return unproven mate or TB scores
-        if (nullValue >= beta && nullValue < VALUE_TB_WIN_IN_MAX_PLY)
+        if (nullValue >= nmpBeta && nullValue < VALUE_TB_WIN_IN_MAX_PLY)
         {
-            if (thisThread->nmpMinPly || depth < 16)
+            if (thisThread->nmpMinPly || depth < 16 - R / 4)
                 return nullValue;
 
             assert(!thisThread->nmpMinPly);  // Recursive verification is not allowed
 
             // Do verification search at high depths, with null move pruning disabled
             // until ply exceeds nmpMinPly.
-            thisThread->nmpMinPly = ss->ply + 3 * (depth - R) / 4;
+            thisThread->nmpMinPly = ss->ply + 3 * (depth - nmpReduction) / 4;
 
-            Value v = search<NonPV>(pos, ss, beta - 1, beta, depth - R, false);
+            Value v = search<NonPV>(pos, ss, beta - 1, beta, depth - nmpReduction, false);
 
             thisThread->nmpMinPly = 0;
 
