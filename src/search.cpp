5fd0e0a70b1a9420d75f3d12c01048728450c7fc
MinetaS <skdty87@gmail.com>
2025-08-30 19:48:59 +0900

    Update TT depth at singular extension cutoffs
    Bench: 2233603

--- src/search.cpp
+++ src/search.cpp
@@ -1143,7 +1143,12 @@ moves_loop:  // When in check, search starts here
             // singular (multiple moves fail high), and we can prune the whole
             // subtree by returning a softbound.
             else if (value >= beta && !is_decisive(value))
+            {
+                if (ttData.depth <= newDepth)
+                    ttWriter.write(posKey, value_to_tt(value, ss->ply), ss->ttPv, BOUND_LOWER,
+                                   newDepth + 1, move, unadjustedStaticEval, tt.generation());
                 return value;
+            }
 
             // Negative extensions
             // If other moves failed high over (ttValue - margin) without the
