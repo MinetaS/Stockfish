41562d5d0672582d2b97390f401c879be389995f
Syine Mineta <skdty87@gmail.com>
2025-09-22 00:58:02 +0900

    Always shuffle at non PV nodes
    Bench: 2285707

--- src/search.cpp
+++ src/search.cpp
@@ -1328,8 +1328,8 @@ moves_loop:  // When in check, search starts here
 
         // In case we have an alternative move equal in eval to the current bestmove,
         // promote it to bestmove by pretending it just exceeds alpha (but not beta).
-        int inc = (value == bestValue && ss->ply + 2 >= rootDepth && (int(nodes) & 14) == 0
-                   && !is_win(std::abs(value) + 1));
+        const bool inc = value == bestValue && ss->ply + 2 >= rootDepth
+                      && (!PvNode || std::uint64_t(nodes) & 0x7) && !is_win(std::abs(value) + 1);
 
         if (value + inc > bestValue)
         {
